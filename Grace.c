f79c11b46e718ead80f27021d5cc2a444c6c363a
overedge <gne-x@hotmail.fr>
2017-02-01 19:01:55 +0100

    Close FD for Grace.c # 2

--- Grace.c
+++ Grace.c
@@ -4,5 +4,5 @@
 /*
  WAWA
 */
-#define FT(x) int main() {char *str = "#include <stdio.h>%c#define TEST 42%c#define blackhat 666%c/*%c WAWA%c*/%c#define FT(x) int main() {char *str = %c%s%c; FILE *fichier = fopen(%cGrace_kid.c%c, %cw%c); fprintf(fichier, str, 10, 10, 10, 10, 10, 10, 34, str, 34, 34, 34, 34, 34, 10, 10);}%cFT(42)%c"; FILE *fichier = fopen("Grace_kid.c", "w"); fprintf(fichier, str, 10, 10, 10, 10, 10, 10, 34, str, 34, 34, 34, 34, 34, 10, 10);}
+#define FT(x) int main() {char *str = "#include <stdio.h>%c#define TEST 42%c#define blackhat 666%c/*%c WAWA%c*/%c#define FT(x) int main() {char *str = %c%s%c; FILE *fichier = fopen(%cGrace_kid.c%c, %cw%c); fprintf(fichier, str, 10, 10, 10, 10, 10, 10, 34, str, 34, 34, 34, 34, 34, 10, 10); fclose(fichier);}%cFT(42)%c"; FILE *fichier = fopen("Grace_kid.c", "w"); fprintf(fichier, str, 10, 10, 10, 10, 10, 10, 34, str, 34, 34, 34, 34, 34, 10, 10); fclose(fichier);}
 FT(42)
